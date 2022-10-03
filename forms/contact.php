ed8ec7e64246842cf441795009a61d7ec8ce89c6
Mduale181 <59575980+Mduale181@users.noreply.github.com>
2022-10-02 19:20:50 -0600

    Add files via upload

---
+++ forms/contact.php
@@ -0,0 +1,17 @@
+<?php
+if (isset($_POST['submit'])){
+  $name = $_POST['name'];
+  $mailFrom = $_POST['email'];
+  $subject = $_POST['subject'];
+  $Message = $_POST['message'];
+
+  $mailto = "deeq15@hotmail.com";
+  $headers = "From: ".$mailFrom;
+  $txt = "You have received an e-mail from ".$name.".\n\n".$Message;
+
+
+  mail($mailto, $subject, $txt, $headers);
+
+  header("Location: index.php?mailsend");
+}
+?>
